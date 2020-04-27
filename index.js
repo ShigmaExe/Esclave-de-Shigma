92c8534286f231b3cd1ebdc504e1f30fa6a4c59d
ShigmaExe <62402256+ShigmaExe@users.noreply.github.com>
2020-04-27 20:48:11 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
+// ~ Distatus - Set a Discord Streaming Status ~
+// Instructions in README.md
 
+const Discord = require("discord.js");
+const client = new Discord.Client();
+
+client.on("ready", () => {
+  console.log(`Logged in as ${client.user.tag}.`);
+  
+  client.user
+    .setActivity("test !", {
+      type: "STREAMING",
+      url: "https://twitch.tv/."
+    })
+    .then(presence =>
+      console.log(
+        `Your Status has been set to "${
+          presence.game ? presence.game : "none"
+        }"`
+      )
+    )
+    .catch(console.error);
+});
+
+client.login("NDI5OTkxNzk1NDI2OTgzOTM2.XkBxcQ.FzTTFGB8p2X38bzFqXCnuw00IIY");
